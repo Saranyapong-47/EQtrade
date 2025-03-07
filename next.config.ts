dae5df58bfec811cf9251296fd907417f85f47a7
Saranyapong Phoksawas <saranyapong.ph@ku.th>
2025-03-08 04:21:13 +0700

    Feature Auth sussec

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  transpilePackages: ['next-auth'],
   experimental: {
-    //appDir: true,
+
   },
+  webpack: (config) => {
+    // แก้ปัญหาการ import module
+    config.resolve.fallback = { ...config.resolve.fallback, fs: false };
+    return config;
+  },
+  
 };
 
 export default nextConfig;
